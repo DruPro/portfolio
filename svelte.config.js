03de0ed0c3248febe0e53687573bcb8cbf84221b
DruPro <buenrostroalan93@gmail.com>
2026-06-24 00:07:42 -0700

    seed

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         adapter: adapter({
             fallback: '404.html'
         }),
         paths: {
-            // Hardcode '/portfolio' if you are deploying to https://drupro.github.io/portfolio/
-            base: '/portfolio',
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
     }
 };
